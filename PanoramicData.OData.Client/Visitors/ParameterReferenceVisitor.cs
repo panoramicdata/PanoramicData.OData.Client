be100bb72ae53b0a489abb1616cdb578a71a4df9
belov-a <belov-a@fbconsult.ru>
2026-03-20 00:49:38 +0700

    add evaluation of non parameterized expression & support odata date functions

---
+++ PanoramicData.OData.Client/Visitors/ParameterReferenceVisitor.cs
@@ -0,0 +1,12 @@
+﻿namespace PanoramicData.OData.Client.Visitors;
+
+internal sealed class ParameterReferenceVisitor : ExpressionVisitor
+{
+	public bool FoundParameter { get; private set; }
+
+	protected override Expression VisitParameter(ParameterExpression node)
+	{
+		FoundParameter = true;
+		return base.VisitParameter(node);
+	}
+}
